cc7dec48100814e9d2e9325564811e5cfb6b8db8
Jack Morse <jack.morse2001@gmail.com>
2023-07-24 19:52:29 +0100

    Test board

--- Board.py
+++ Board.py
@@ -12,7 +12,7 @@ class Board:
             [Piece(Material.ROOK, Alliance.BLACK), Piece(Material.KNIGHT, Alliance.BLACK), Piece(Material.BISHOP, Alliance.BLACK), Piece(Material.QUEEN, Alliance.BLACK),
              Piece(Material.KING, Alliance.BLACK), Piece(Material.BISHOP, Alliance.BLACK), Piece(Material.KNIGHT, Alliance.BLACK), Piece(Material.ROOK, Alliance.BLACK)],
             [Piece(Material.PAWN, Alliance.BLACK) for _ in range(8)],
-            ([None for _ in range(8)] for _ in range(4)),
+            [None for y in range(8)] for x in range(4),
             [Piece(Material.PAWN, Alliance.WHITE) for _ in range(8)]
             [Piece(Material.ROOK, Alliance.WHITE), Piece(Material.KNIGHT, Alliance.WHITE), Piece(Material.BISHOP, Alliance.WHITE), Piece(Material.QUEEN, Alliance.WHITE),
              Piece(Material.KING, Alliance.WHITE), Piece(Material.BISHOP, Alliance.WHITE), Piece(Material.KNIGHT, Alliance.WHITE), Piece(Material.ROOK, Alliance.WHITE)]            
